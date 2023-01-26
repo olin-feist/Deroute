f996e512a0b40b4fa32b8eed56d30c576429516a
feisto <feisto@wwu.edu>
2023-01-25 17:52:56 -0800

    change parser to take urls

--- build_dataset.sh
+++ build_dataset.sh
@@ -1,6 +1,5 @@
 #get website and parse it
-curl -s -L -o data/webhtml.html https://en.wikipedia.org/wiki/Curse_of_dimensionality
-python src/parse_website.py data/webhtml.html
+python src/parse_website.py https://en.wikipedia.org/wiki/Curse_of_dimensionality
 
 #embedding
 bin/embed.exe bin/model.en.bin data/website_plain.txt data/vectors.bin new
@@ -8,8 +7,7 @@ echo https://en.wikipedia.org/wiki/Curse_of_dimensionality
 
 #repeat for all urls in urls.txt
 while read -r line; do
-    curl -s -L -o data/webhtml.html "$line"
-    python src/parse_website.py data/webhtml.html
+    python src/parse_website.py "$line"
     bin/embed.exe bin/model.en.bin data/website_plain.txt data/vectors.bin append
     echo "$line"
 done < "data/urls.txt"
