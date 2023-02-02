f736ca4ce619091b60ca45d608ea114181e08166
olin-feist <olin.feist@gmail.com>
2023-02-01 17:35:32 -0800

    updated testing scripts

--- build_dataset.sh
+++ build_dataset.sh
@@ -1,13 +1,38 @@
-#get website and parse it
-python src/parse_website.py https://en.wikipedia.org/wiki/Curse_of_dimensionality
+rm data/vectors.bin
+echo -e "\033[31m- Deleted vectors.bin\033[0m"
 
-#embedding
-bin/embed.exe bin/model.en.bin data/website_plain.txt data/vectors.bin new
-echo https://en.wikipedia.org/wiki/Curse_of_dimensionality
+filename=$1
 
-#repeat for all urls in urls.txt
+# Count the number of lines in the file
+lines=$(wc -l < $filename)
+
+# Loop through the lines of the file
+i=0
 while read -r line; do
-    python src/parse_website.py "$line"
-    bin/embed.exe bin/model.en.bin data/website_plain.txt data/vectors.bin append
-    echo "$line"
-done < "data/urls.txt"
+
+  # Embed urls
+  python src/parse_website.py "$line"
+  bin/embed.exe  bin/model.en.bin data/vectors.bin < data/website_plain.txt
+
+  i=$((i+1))
+
+  # Calculate the percentage completed
+  percent=$((100 * $i / $lines))
+
+  # Output the progress bar
+  echo -ne "\r["
+  for j in $(seq 1 10); do
+    if [ $j -le $((percent / 10)) ]; then
+      echo -ne "\033[32m#\033[0m"
+    else
+      echo -ne "\033[31m-\033[0m"
+    fi
+  done
+  echo -ne "] $percent%"
+
+
+  echo -e "\033[33m \tAdded URL ($i/$lines):\033[0m" "$line"
+
+done < $filename
+echo -ne "\n"
+echo -e "\033[32m- Built Dataset of $lines URLs \033[0m" 
