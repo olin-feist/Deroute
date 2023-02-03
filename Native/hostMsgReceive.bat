d3824b3713dd3cb354eba149a7198d0c5fe042e2
Cameron-Evans <59668002+Cameron-Evans@users.noreply.github.com>
2023-02-03 11:25:32 -0800

    Native messaging happy now

--- Native/hostMsgReceive.bat
+++ Native/hostMsgReceive.bat
@@ -4,6 +4,6 @@ set LOG=%~dp0controller.log
 
 time /t >> %LOG%
 
-"node.exe" "%~dp0controller.js" %* 2>> %LOG%
+"node.exe" "%~dp0hostMsgReceive.js" %* 2>> %LOG%
 
 echo %errorlevel% >> %LOG%
--- Native/hostMsgReceive.js
+++ Native/hostMsgReceive.js
@@ -14,13 +14,19 @@ process.stdin
 function messageHandler(msg, push, done) {
     if (msg.url) {
         //give to fastText
+        push(msg);
         done();
     }else if (msg.query) {
         //give to swarm
+        push(msg);
         done();
     } else {
         // Just echo the message:
         push(msg);
         done();
     }
+}
+
+function receiveSwarmResponse(response) {
+    //give to extension
 }
