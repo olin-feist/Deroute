4499483732631804759a90c23b6765f8981c1d77
Cameron-Evans <59668002+Cameron-Evans@users.noreply.github.com>
2023-02-03 11:25:32 -0800

    Native messaging happy now

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,69 +1,86 @@
-(function() {
 
-    var application = 'deroute_controller';
-    var port = null;
 
-    document.getElementById('connect').addEventListener('click', function() {
-        log('chrome.runtime.connectNative')
+var application = 'deroute_controller';
+var port = null;
 
+document.getElementById('reconnect').addEventListener('click', reconnect);
+document.getElementById('disconnect').addEventListener('click', disconnect);
+document.getElementById('send').addEventListener('click', function() {send(document.getElementById('msg').value)});
+document.getElementById('clear').addEventListener('click', function() {
+    document.getElementById('log').innerHTML = '';
+});
+
+
+
+function reconnect() {
+    disconnect();
+    log('Attempting connection with native application');
+
+    try {
         port = chrome.runtime.connectNative(application);
-
-        port.onMessage.addListener(log);
-
-        port.onDisconnect.addListener(function(e) {
-            log('unexpected disconnect');
-
-            port = null;
-        });
-    });
-
-    document.getElementById('disconnect').addEventListener('click', function() {
-        log('port.disconnect');
-        port.disconnect(); // this doesn't seem to trigger the onDisconnect event
-        port = null;
-    });
-
-    var examples = {
-        ping: { ping: 'pong' },
-        url: { url: 'https://stackoverflow.com/questions/tagged/fibonacci' },
-        query: { query: 'good breakfast foods?' }
-    };
-
-    Array.prototype.slice.call(document.querySelectorAll('[data-example]')).forEach(function(example) {
-        example.addEventListener('click', function() {
-            document.getElementById('msg').value = JSON.stringify(examples[example.dataset.example]);
-        });
-    });
-
-    document.getElementById('send').addEventListener('click', function() {
-        var json = document.getElementById('msg').value;
-        var msg;
-
-        try {
-            msg = JSON.parse(json);
-        } catch (err) {
-            return log('invalid JSON: ' + json);
-        }
-
-        if (port) {
-            log('port.postMessage');
-            port.postMessage(msg);
-        } else {
-            log('chrome.runtime.sendNativeMessage');
-            chrome.runtime.sendNativeMessage(application, msg, log);
-        }
-    });
-
-    document.getElementById('clear').addEventListener('click', function() {
-        document.getElementById('log').innerHTML = '';
-    });
-
-    function log(msg) {
-        console.log(msg);
-
-        var e = document.createElement('pre');
-        e.appendChild(document.createTextNode(typeof msg === 'object' ? JSON.stringify(msg) : msg));
-        document.getElementById('log').appendChild(e);
+    } catch {
+        return log("Failed to connect with native application");
     }
 
-})();
+    port.onMessage.addListener(receiveMsg);
+
+    port.onDisconnect.addListener(function(e) {
+        log('Unexpected disconnect with native application');
+        port = null;
+    });
+}
+
+function disconnect() {
+    try {
+        port.disconnect();
+        log('port.disconnect');// this doesn't seem to trigger the onDisconnect event
+        port = null;
+    } catch {
+
+    }
+}
+
+function send(msgAsJson) {
+    var msg;
+
+    try {
+        msg = JSON.parse(msgAsJson);
+    } catch (err) {
+        return log('invalid JSON: ' + json);
+    }
+
+    if (!port) {
+        reconnect();
+        if (!port) {
+            return log("Failed to send: " + msg);
+        }
+    }
+    port.postMessage(msg);
+    log('Sent: ');
+    log(msg);
+}
+
+function receiveMsg(msg){
+    log("Recevied: ")
+    log(msg);
+}
+
+function log(msg) {
+    console.log(msg);
+
+    var e = document.createElement('pre');
+    e.appendChild(document.createTextNode(typeof msg === 'object' ? JSON.stringify(msg) : msg));
+    document.getElementById('log').appendChild(e);
+}
+
+var examples = {
+    ping: { ping: 'pong' },
+    url: { url: 'https://stackoverflow.com/questions/tagged/fibonacci' },
+    query: { query: 'good breakfast food?' }
+};
+
+Array.prototype.slice.call(document.querySelectorAll('[data-example]')).forEach(function(example) {
+    example.addEventListener('click', function() {
+        document.getElementById('msg').value = JSON.stringify(examples[example.dataset.example]);
+    });
+});
