8edfb38ae3133109a3f8893a7894f235ddf6e2fe
Cameron-Evans <59668002+Cameron-Evans@users.noreply.github.com>
2023-02-23 14:14:09 -0800

    express server workin p well

--- Native/hostMsgReceive.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set LOG=%~dp0controller.log
-
-time /t >> %LOG%
-
-"node.exe" "%~dp0hostMsgReceive.js" %* 2>> %LOG%
-
-echo %errorlevel% >> %LOG%
--- Native/node_modules/chrome-native-messaging/host/register.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-mkdir -p /Library/Google/Chrome/NativeMessagingHosts
-cp com.my_company.my_application.json /Library/Google/Chrome/NativeMessagingHosts
