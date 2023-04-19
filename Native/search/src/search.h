175fa26f928580676405575b633038ef8228de35
feisto <feisto@wwu.edu>
2023-04-18 18:40:55 -0700

    updated file structure

---
+++ Native/search/src/search.h
@@ -0,0 +1,90 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fcntl.h>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include <faiss/IndexFlat.h>
+#include <faiss/impl/AuxIndexStructures.h>
+
+#include "utils.h"
+
+/**
+ * Return type for search
+ * @param k          number of results found
+ * @param distances  pointer to list of distance
+ * @param urls       pointer to list of urls
+ */
+struct search_ret { 
+    int k;
+    float* distances;
+    char** urls;
+
+    ~search_ret();
+};
+
+
+/**
+ * Class the contains all the data for Query - > Vectors - > Websites 
+ * Function call info is the same as their corresponding python wrapper calls,
+ * i.e update_index() <-> update() have the same documentation
+ */
+class URLVectorIndex{
+    private:
+        faiss::IndexFlatIP search_index;    // FAISS index that contains dense vectors
+        std::vector<std::string> urls;      // List of urls that label the FAISS index
+        int d;                              // Dimension
+        int nb;                             // entries
+        int nq;                             // number of queries
+        char* vectors_path;                 // Path to dense vectors
+        char* urls_path;                    // Path to URLS
+        bool isLoaded;                      // Status of database
+
+    
+    public:
+        URLVectorIndex();
+        ~URLVectorIndex();
+        int load(char* vectors_p, char* urls_p);
+        int update();
+        search_ret* search(float* queries);
+        
+};
+
+
+using idx_t = faiss::Index::idx_t;  // FAISS 64-bit int
+extern URLVectorIndex database;            // Database of Websites
+
+// Python Wrappers
+extern "C" {
+    /**
+     * Update the current database in memory with new content from local file
+     * @return              1 success -1 Error
+     */
+    int update_index();
+
+    /**
+     * Load vectors and their corresponding labels (URLS) into memory
+     * @param vectors_p     path to .bin of vectors
+     * @param urls_p        path to .bin of urls
+     * @return              1 success -1 Error
+     */
+    extern "C"
+    int load_data(char* vectors_p, char* urls_p);
+
+    /**
+     * Perform similarity search
+     * @param queries       vector of single query
+     * @return struct containing results and distances
+     */
+    search_ret* search(float* queries);
+
+    //for python wrappers to dealloc pointers
+    void free_mem(void* ptr);
+
+    //for python wrappers to dealloc list of pointers
+    void free_list(void** ptr,int k);
+
+    //for python wrappers to dealloc return results
+    void delete_struct(search_ret* ptr);
+}
---
+++ Native/search/src/utils.cpp
@@ -0,0 +1,68 @@
+#include "utils.h"
+
+int utils::print_vectors(std::string path){
+    std::fstream file(path , std::ios::in | std::ios::binary);
+    if(!file) {
+        std::cerr << "Cannot open file!" << std::endl;
+        return 1;
+    }
+
+    std::string str; 
+    int dimension;
+    file.read((char*) &dimension, sizeof(int));
+    std::cout<<dimension<<" ";
+    int elements;
+    
+    file.read((char*) &elements, sizeof(int));
+    std::cout<<elements<<std::endl;
+    float* vec= new float[dimension];
+    for(int i=0;i<elements;i++){
+        file.read((char*) vec, sizeof(float)*dimension);
+        for(int i=0;i<15;i++)
+            std::cout<<vec[i]<<", ";
+        std::cout<<"..."<<std::endl;
+    }
+    delete(vec);
+
+    return 0;
+        
+}
+
+int utils::mul_vector(float* v1, float mult, int size){
+    for (int i = 0; i < size; i++) {
+        v1[i] *= mult;
+    }
+    return 0;
+}
+
+int utils::add_vectors(float* v1, float* v2, int size){
+    for (int i = 0; i < size; i++) {
+        v1[i] += v2[i];
+    }
+    return 0;
+}
+
+float* utils::read_vectors(std::string path, int *d, int *n){
+    std::ifstream file;
+
+    file.open(path,std::ios::binary);
+    if(!file) {
+        std::cerr << "Cannot open file!" << std::endl;
+        return NULL;
+    } 
+    int dimensions;
+    int entrys;
+    file.read((char*) &dimensions, sizeof(int)); // dimension
+    file.read((char*) &entrys, sizeof(int)); // database size
+    float *vec = new float[dimensions*entrys];
+    
+    //get database
+    for(int i=0;i<entrys;i++){
+        file.read((char*) &vec[dimensions*i], sizeof(float)*dimensions);
+    }
+
+    file.close();
+    *d=dimensions;
+    *n=entrys;
+    return vec;
+}
